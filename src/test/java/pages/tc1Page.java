23a7fa18f97950d524d8265843542437328229d9
Maximiliano Reyes López <113549509+mmaaxi@users.noreply.github.com>
2025-04-22 01:48:05 -0600

    agregando src/test/java/pages/tc1Page.java

---
+++ src/test/java/pages/tc1Page.java
@@ -0,0 +1,20 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class tc1Page {
+    WebDriver driver;
+
+    public tc1Page(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void realizarAccion1() {
+        // Implementación para realizar la Acción 1
+    }
+
+    public boolean verificarResultado1() {
+        // Implementación para verificar el Resultado 1
+        return true;
+    }
+}
