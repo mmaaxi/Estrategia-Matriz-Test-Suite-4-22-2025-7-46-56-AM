94af0636e59bcb1da8bb8adf00a2bada2db2690f
Maximiliano Reyes López <113549509+mmaaxi@users.noreply.github.com>
2025-04-22 01:48:07 -0600

    agregando src/test/java/pages/tc4Page.java

---
+++ src/test/java/pages/tc4Page.java
@@ -0,0 +1,21 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class tc4Page {
+
+    WebDriver driver;
+
+    public tc4Page(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void realizarAccion4() {
+        // código para realizar Acción 4
+    }
+
+    public boolean verificarResultado4() {
+        // código para verificar Resultado 4
+        return true;
+    }
+}
